ab4e004044cdf71c31296ad04a3d252bdd9a3fc4
Andrey Helldar <helldar@ai-rus.com>
2021-07-26 14:34:39 +0300

    Fixed: `Return type declaration must be compatible with Client->getClientId() : string`

--- src/Cashier/Authentication/Client.php
+++ src/Cashier/Authentication/Client.php
@@ -6,11 +6,11 @@ interface Client
 {
     public function setClientId(?string $client_id): self;
 
-    public function getClientId(): string;
+    public function getClientId(): ?string;
 
     public function setClientSecret(?string $client_secret): self;
 
-    public function getClientSecret(): string;
+    public function getClientSecret(): ?string;
 
     public function doesntEmpty(): bool;
 }
