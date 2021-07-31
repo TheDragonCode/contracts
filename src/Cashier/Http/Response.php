5e77819909af288fafebac57cda99cbfcf845d27
Andrey Helldar <helldar@ai-rus.com>
2021-08-01 00:27:29 +0300

    Removed `Jsonable` contract from Cashier Response

--- src/Cashier/Http/Response.php
+++ src/Cashier/Http/Response.php
@@ -6,10 +6,9 @@ namespace Helldar\Contracts\Cashier\Http;
 
 use Helldar\Contracts\DataTransferObject\DataTransferObject;
 use Helldar\Contracts\Support\Arrayable;
-use Helldar\Contracts\Support\Jsonable;
 
 /** @method static Response make(array $items = []) */
-interface Response extends DataTransferObject, Arrayable, Jsonable
+interface Response extends DataTransferObject, Arrayable
 {
     public function getExternalId(): ?string;
 
