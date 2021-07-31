1d4b2d5872afc5b7b103191935cc66bfee1f3a9f
Andrey Helldar <helldar@ai-rus.com>
2021-07-31 17:41:08 +0300

    Update Auth.php

--- src/Cashier/Auth/Auth.php
+++ src/Cashier/Auth/Auth.php
@@ -7,7 +7,7 @@ namespace Helldar\Contracts\Cashier\Auth;
 use Helldar\Contracts\Cashier\Http\Requests\Request;
 use Helldar\Contracts\Cashier\Resources\Model;
 
-/** @method static Auth make() */
+/** @method static Auth make(Model $model, Request $request, bool $hash = true) */
 interface Auth
 {
     public function __construct(Model $model, Request $request, bool $hash = true);
