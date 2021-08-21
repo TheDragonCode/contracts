2402212b1c694f4b630de61e8e5b29d0f078203f
Andrey Helldar <helldar@ai-rus.com>
2021-08-21 16:10:56 +0300

    Added the ability to pass additional data to Cachier Auth contract

--- src/Cashier/Auth/Auth.php
+++ src/Cashier/Auth/Auth.php
@@ -10,7 +10,7 @@ use Helldar\Contracts\Cashier\Resources\Model;
 /** @method static Auth make(Model $model, Request $request, bool $hash = true) */
 interface Auth
 {
-    public function __construct(Model $model, Request $request, bool $hash = true);
+    public function __construct(Model $model, Request $request, bool $hash = true, array $extra = []);
 
     public function headers(): array;
 
