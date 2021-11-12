0a60d64edf1721d13fb1988f645b383248645617
Andrey Helldar <helldar@ai-rus.com>
2021-11-13 00:54:16 +0300

    Create Store.php

---
+++ src/Cache/Store.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the "dragon-code/contracts" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/TheDragonCode/contracts
+ */
+
+declare(strict_types=1);
+
+namespace DragonCode\Contracts\Cache;
+
+interface Store
+{
+    /**
+     * Retrieve an item from the cache by key.
+     *
+     * @param  string  $key
+     *
+     * @return mixed
+     */
+    public function get(string $key);
+
+    /**
+     * Store an item in the cache for a given number of seconds.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  int  $seconds
+     *
+     * @return mixed
+     */
+    public function put(string $key, $value, int $seconds);
+
+    /**
+     * Remove an item from the cache.
+     *
+     * @param  string  $key
+     *
+     * @return mixed
+     */
+    public function forget(string $key);
+}
